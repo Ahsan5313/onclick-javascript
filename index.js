821356a4087b32a8a3bba8764d224b0ecf019083
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-10 11:30:43 +0600

    add javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+function myMassage(){
+    alert("hi i am button");
+}
+
+function myMassage1(){
+    alert("hi i am button1");
+}
+function heaed(){
+    alert("hello everyone i am head");
+}
+function para(){
+    alert("hi i am paragaraph ");
+}
